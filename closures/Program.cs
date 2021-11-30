41b51a43940d1f8ace7f2eca7e50f3bc662fd288
unknown <nishan.rumc@gmail.com>
2021-11-30 23:18:19 +0600

    addclosure

---
+++ closures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//closures
+int CalculateSomething(int n)
+{
+    if (n == 0) return 0;
+    var factor = 42;
+    return factor * CalculateSomething(n-1);
+}
+
+Func<int, int> Calculator = CreateCalculator();
+Console.WriteLine(Calculator(10));
+
+
+Func<int, int> CreateCalculator()
+{
+    var factor = 42;
+    return n => n * factor;
+}
---
+++ p3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+//no parameter and return type void empty function
+Action a=()=>Console.WriteLine("hi");
+a();
+//one int parameter and return void
+Action<int> a1=n=>Console.WriteLine(n*n);
+a1(10);
+//two parameter 
+Action<string,string> a2=(s1 ,s2)=>Console.WriteLine(s1+s2);
+a2("Hello","World");
+//func delegate  no parameter but return int
+Func<int> f=()=>12;
+Console.WriteLine(f());
+//func with int parameter and return int 
+//here it always return last type of parameter here int
+Func<int ,int> f1=n=>n*n;
+Console.WriteLine(f1(10));
+//here return bool the last parameter
+Func<int,int,bool> f2=(n1,n2)=>n1==n2;
+Console.WriteLine(f2(10,20));
